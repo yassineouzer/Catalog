4f0bab4e3c4e63dd38dbf010c978a88a6e40edcb
ouzeren yassine <49686960+yassineouzer@users.noreply.github.com>
2022-10-04 11:52:11 +0100

    methode ngOninit()

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { ProductsComponent } from './products/products.component';
 import { CustomersComponent } from './customers/customers.component';
 
 const routes: Routes = [
-  {path:"products", component:ProductsComponent},
-  {path:"customers", component:CustomersComponent},
+  {path:"products", component: ProductsComponent},
+  {path:"customers", component: CustomersComponent},
 ];
 
 @NgModule({
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
+  products! : Array<any>;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.products=[
+    {id:1,name:"computer", price:6500},
+    {id:2,name:"imprimante", price:1400},
+    {id:1,name:"smartphone", price:1200},
+    ]
+
+    
+
   }
 
 }
